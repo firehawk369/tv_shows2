b6fb84933ca44c3e02510e3cebe60a7d6225fdd5
Keegan Whitener <kw0922760@otc.edu>
2020-12-11 14:21:38 -0600

    initial commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    <Cards class="card" style="width: 18rem;">
+      <img v-bind:src="card.imgSrc" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{{card.name}}</h5>
+        <p class="card-text">{{card.description}}</p>
+      </div>
+    </Cards>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Cards',
+    props:['card']
+}
+</script>
+<!--Keegan Whitener-->
+<!--This page was suppose to put out the movie cards, but I couldn't seem to find out what it meant by imgSrc not definded even though the infomation was added into the code via mounted even before the Cards.vue even ran.-->
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+    <h1>Popular Tv Shows</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background: blue;
+  color: white;
+  text-align: center;
+  padding: 10px;
+  margin: 0px;
+}
+</style>
+
+<!--Keegan Whitener-->
+<!--This page is just the header of the page-->
